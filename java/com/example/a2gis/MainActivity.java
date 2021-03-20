33e9f5ddb0cd8b48c8920cd0bb2e6a1bc26b24d7
nikita ulyanov <nikita.ulyanov.2002@inbox.ru>
2021-03-20 10:30:55 +0900

    123

---
+++ java/com/example/a2gis/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.a2gis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+
+        webView.loadUrl("file:///android_asset/main.html");
+
+        WebViewClient webViewClient = new WebViewClient() {
+
+            @SuppressWarnings("deprecation") @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                view.loadUrl(url);
+
+                return true;
+
+            }
+
+
+
+            @TargetApi(Build.VERSION_CODES.N) @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                view.loadUrl(request.getUrl().toString());
+
+                return true;
+
+            }
+
+        };
+        webView.setWebViewClient(webViewClient);
+    }
+}
