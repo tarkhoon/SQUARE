9270e1087a5eb5ab0910afd78974d9f313c4fa58
nikita ulyanov <nikita.ulyanov.2002@inbox.ru>
2021-03-21 15:52:40 +0900

    213

---
+++ assets/js/profile.js
@@ -0,0 +1,6 @@
+function avatar(){
+    document.getElementById("avatar").className ='avatar-active';
+    document.getElementById("photo").className ='photo-non-active';
+    document.getElementById("reviews").className ='reviews-non-active';
+    document.getElementById("clarification").className ='clarification-non-active';
+}
